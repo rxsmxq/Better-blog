1e9c41c83e63123a67c6a826bd61764dc04daea8
MmMing <784774835@qq.com>
2026-05-23 03:13:33 +0800

    feat(friends config): add two new friend links
    Add UpXuu's blog and 椰汁の博客 to the friend link list

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -138,6 +138,24 @@ export const friendsConfig: FriendLink[] = [
 		weight: 5,
 		enabled: true,
 	},
+	{
+		title: "UpXuu's blog",
+		imgurl: "https://upxuu.com/images/20260214145619.jpg",
+		desc: "逐光而上",
+		siteurl: "https://upxuu.com/",
+		tags: ["Blog"],
+		weight: 5,
+		enabled: true,
+	},
+	{
+		title: "椰汁の博客",
+		imgurl: "https://free.picui.cn/free/2026/03/23/69c12fe83f7a4.jpg",
+		desc: " 关关难过关关过,前路漫漫亦灿灿. ",
+		siteurl: "https://home.132614.xyz/",
+		tags: ["Blog"],
+		weight: 5,
+		enabled: true,
+	},
 ];
 
 // 获取启用的友链并进行排序
