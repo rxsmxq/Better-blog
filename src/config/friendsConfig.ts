ff7cf4f79cef865087f341beed39e658e4318fcf
MmMing <784774835@qq.com>
2026-06-18 10:44:50 +0800

    feat(friendsConfig): add 阿叶Ayeez的小站 as friend link

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -212,6 +212,15 @@ export const friendsConfig: FriendLink[] = [
 		weight: 5,
 		enabled: true,
 	},
+	{
+		title: "阿叶Ayeez的小站",
+		imgurl: "https://qiniu.ayeez.cn/avatar.jpg",
+		desc: "记录学习历程，记录美好生活",
+		siteurl: "ttps://blog.Ayeez.cn",
+		tags: ["Blog"],
+		weight: 5,
+		enabled: true,
+	},
 ];
 
 // 获取启用的友链并进行排序
